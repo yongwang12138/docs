4914bbcf9f984698191a675af46397e88ba98831
wangyongwang <13327418643@163.com>
2025-10-12 17:24:48 +0800

    增加Beyond Compare重置试用期。
    Signed-off-by: wangyongwang <13327418643@163.com>

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
         text: '教程', items: [
           { text: 'VitePress博客搭建', link: '/tutorial/vitepress' },
           { text: 'Git使用教程', link: '/tutorial/git' },
+          { text: 'Beyond Compare', link: '/tutorial/compare' },
         ]
       }
     ],
@@ -32,7 +33,7 @@ export default defineConfig({
             items: [
               { text: 'VitePress博客搭建', link: '/tutorial/vitepress' },
               { text: 'Git使用教程', link: '/tutorial/git' },
-              { text: 'Linux脚本自启动', link: '/tutorial/script-startup' },
+              { text: 'Beyond Compare重置试用期', link: '/tutorial/compare' },
             ],
         },
       ],
---
+++ tutorial/compare.md
@@ -0,0 +1,19 @@
+# Beyond Compare重置试用期
+
+## 1. 打开注册表
+
+- Win+R输入regedit
+
+![image-20251012171624663](compare/image-20251012171624663.png)
+
+## 2. 打开Beyond Compare注册表路径
+
+```bash
+计算机\HKEY_CURRENT_USER\SOFTWARE\Scooter Software\Beyond Compare 5
+```
+
+![image-20251012172226076](compare/image-20251012172226076.png)
+
+## 3. 删除CacheID项
+
+![image-20251012172107393](compare/image-20251012172107393.png)
